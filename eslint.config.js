ac0562617d62f3b80acfffd0916667656c7199cc
Fitsumm <m.fitsummehari@gmail.com>
2026-07-24 11:46:09 +0300

    chore: add ESLint flat config

---
+++ eslint.config.js
@@ -0,0 +1,18 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ["dist/**", "node_modules/**", "scripts/**", "public/sw.js"],
+  },
+  {
+    files: ["src/**/*.ts"],
+    rules: {
+      "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "@typescript-eslint/consistent-type-imports": "error",
+      "no-console": ["warn", { allow: ["warn", "error", "log"] }],
+    },
+  },
+);
